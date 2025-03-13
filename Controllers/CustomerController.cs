c8493ccc3c882074652b99896b4769714bcb6f07
kunal pawar <kunalspawar2191@gmail.com>
2025-03-13 16:22:08 +0530

    Changes Made in Landing Page - 13.03.2025

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Proj1.DbContext;
 
 namespace Proj1.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext _context; 
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Landing()
         {
-            return View();
+
+           var results = _context.Roles.ToList();
+           return View();
         }
 
         public IActionResult Cart()
